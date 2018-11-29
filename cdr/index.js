9ab6e8885b2db2d782c5a55871c1b4c0fba6c1c6
karthik <kbibireddy@windhoverlabs.com>
2018-11-29 10:15:05 -0600

    web plugin for hs

--- cdr/index.js
+++ cdr/index.js
@@ -1,37 +1,37 @@
 'use strict';
 
-var path = require('path');
+var path = require( 'path' );
 
-const CdrPlugin = require(path.join(global.CDR_INSTALL_DIR, '/commander/classes/CdrPlugin')).CdrPlugin;
+const CdrPlugin = require( path.join( global.CDR_INSTALL_DIR, '/commander/classes/CdrPlugin' ) ).CdrPlugin;
 
 module.exports = class CfeCdrPlugin extends CdrPlugin {
-  constructor(urlBase) {
-    super('amc', path.join(__dirname, 'web', urlBase));
+  constructor( urlBase ) {
+    super( 'hs', path.join( __dirname, 'web', urlBase ) );
   }
 
   getContent() {
     var result = {
-      shortDescription: 'Actuator and Motor Control',
-      longDescription: 'Actuator and Motor Control.',
+      shortDescription: 'HS',
+      longDescription: 'Health Services.',
       nodes: {
-				main: {
-					type: CdrPlugin.ContentType.LAYOUT,
-					shortDescription: 'Main',
-					longDescription: 'Main AMC.',
-					filePath: '/main_layout.lyt'
-				},
-				cdh: {
-					type: CdrPlugin.ContentType.PANEL,
-					shortDescription: 'Command and Data Handling',
-					longDescription: 'Command counters.',
-					filePath: '/cdh.pug'
-				},
-                ao: {
-                    type: CdrPlugin.ContentType.PANEL,
-                    shortDescription: 'Actuator Outputs',
-                    longDescription: 'Actuator Outputs',
-                    filePath: '/ao.pug'
-                }
+        main: {
+          type: CdrPlugin.ContentType.LAYOUT,
+          shortDescription: 'Main',
+          longDescription: 'Main HS.',
+          filePath: '/main.lyt'
+        },
+        cdh: {
+          type: CdrPlugin.ContentType.PANEL,
+          shortDescription: 'Command and Data Handling',
+          longDescription: 'Command counters.',
+          filePath: '/cdh.pug'
+        },
+        exec: {
+          type: CdrPlugin.ContentType.PANEL,
+          shortDescription: 'Execution Counts',
+          longDescription: 'Execution Counts',
+          filePath: '/exec.pug'
+        }
       }
     };
 
