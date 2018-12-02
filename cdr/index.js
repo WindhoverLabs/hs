ad3a033192dcaea9720357a7aadedf4ec563a8be
kb <kbibireddy@windhoverlabs.com>
2018-12-02 17:36:04 -0600

    added proper names

--- cdr/index.js
+++ cdr/index.js
@@ -11,7 +11,7 @@ module.exports = class CfeCdrPlugin extends CdrPlugin {
 
   getContent() {
     var result = {
-      shortDescription: 'HS',
+      shortDescription: 'Health Services',
       longDescription: 'Health Services.',
       nodes: {
         main: {
